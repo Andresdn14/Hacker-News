d0ff81713fd306171712385c71a1f2b0db4820a9
Andres Felipe De La Ossa Navarro <andresdelaossa@hotmail.com>
2020-04-10 17:39:17 -0500

    Initial Commit

---
+++ Hacker News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hacker News
+//
+//  Created by Andres Felipe De La Ossa Navarro on 4/10/20.
+//  Copyright © 2020 Andres Felipe De La Ossa Navarro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
